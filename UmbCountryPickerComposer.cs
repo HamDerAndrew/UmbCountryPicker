010b8f3c7d8e21a49a6c2ea58f644eb96c7ff421
Simon Steed <hello@simonantony.co.uk>
2025-11-14 11:36:28 +0000

    Fix incorrect namespace names Add model for new dto Add new config file Add composer

---
+++ UmbCountryPickerComposer.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using UmbCountryPicker.Models;
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.DependencyInjection;
+
+namespace UmbCountryPicker;
+
+public class UmbCountryPickerComposer : IComposer
+{
+    public void Compose(IUmbracoBuilder builder)
+    {
+        builder.Services.AddSingleton(provider =>
+        {
+            var env = provider.GetService<IWebHostEnvironment>();
+            var configPath = Path.Combine(
+                env.ContentRootPath,
+                "App_Plugins",
+                "UmbCountryPicker",
+                "defaultOverrides.json");
+
+            if (!File.Exists(configPath))
+                return new UmbCountryPickerConfig(); // empty config, safe fallback
+
+            var json = File.ReadAllText(configPath);
+
+            return JsonSerializer.Deserialize<UmbCountryPickerConfig>(json)
+                   ?? new UmbCountryPickerConfig();
+        });
+    }
+}
